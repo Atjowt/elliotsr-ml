7caa4083ce7ffb959367df089287062dcd4cf717
Elliot Skogsberg <elliot.skogsberg@gmail.com>
2023-11-30 19:33:41 +0100

    add files

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/atjowt/External Disk/repo/CNN
-# Build directory: /media/atjowt/External Disk/repo/CNN/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
